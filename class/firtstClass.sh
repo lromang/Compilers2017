dfbf721bf45f6e55a5492d256f399c7851b2829d
lromang <luis.roangarci@gmail.com>
2017-01-16 12:43:57 -0600

    first homework

--- class/firtstClass.sh
+++ class/firtstClass.sh
@@ -1,10 +1,37 @@
 #! /bin/bash
 
+########################################
+## ---------------------------------
+## Author: Luis Manuel Román García
+## ---------------------------------
+##
+## This scripts receives a string
+## that represents a sum of strings
+##
+## -flags-
+## -i: Executes the sum.
+## -c: Generates a C script that
+##     performes the sum.
+##
+########################################
 
+# Check correct format
+if [ -z "$2" ]
+then
+    echo "Falta un argumento!"
+    echo "uso: ./firstClass.sh -i/-c string_of_numbers"
+fi
 
-# Read in numbers
+# Read in string representing a sum
+valid=$(echo $2 | grep -Eo '((^[1-9][0-9]*)\+([1-9][0-9]*))+')
+
+if [ "$valid" != "$2" ]
+then
+    echo 'La cadena no representa una suma de enteros!'
+    exit -1
+fi
+
 string=$(echo $2 | sed -r 's/ +//g')
-
 
 if [ "$1" == "-i" ]
 then
@@ -14,6 +41,7 @@ else if [ "$1" == "-c" ]
          echo -e "#include <stdio.h> \n int main(){\n int a = $string;" > test.c
          echo -e '\n printf("El resultado de la suma es: %d ", a);\n}' >> test.c
      else
-         echo 'Opción no reconocida'
+         echo 'Instrucción no reconocida'
+         echo "uso: ./firstClass.sh -i/-c string_of_numbers"
      fi
 fi
